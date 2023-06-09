ecd025e8e15b0ef01aaff208ecb062693e399ac3
Rameesha Khan <rameeshakhan75@gmail.com>
2023-06-09 16:39:31 +0500

    Add User form created

--- React-Projects/Project1/src/components/User/AddUser.jsx
+++ React-Projects/Project1/src/components/User/AddUser.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState} from 'react';
 
-const AddUser = () => {
+const AddUser = (props) => {
+  const [userDetails, setUserDetail] = useState({
+    username: '',
+    age: '',
+    gender: ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(userDetails.username ==="" || userDetails.age=== "" || userDetails.gender === ""  ){
+      alert ("Plase Enter all fields")
+      return
+    }
+    if(userDetails.age <=0){
+      alert ("Age cant be negative and zero ")
+      return
+    }
+    props.onAddUser(userDetails.username, userDetails.age , userDetails.gender)
+    setUserDetail({
+      username: '',
+      age: '',
+      gender: ''
+    });
+  };
+
+  const handleChange = (event) => {
+    setUserDetail((previousState) => ({
+      ...previousState,
+      [event.target.name]: event.target.value
+    }));
+  };
+
   return (
-     <form>
-      <label className='label'>Username</label>
-      <input className="input" type="text"/>
-      <label className='label'>Age</label>
-      <input className="input" type="number"/>
-      <input className='button' type="submit" value="Add User" />
-    </form>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit}>
+        <label className="label">Username</label>
+        <input className="input" type="text" name="username" value={userDetails.username} onChange={handleChange} />
+        <label className="label">Age</label>
+        <input className="input" type="number" name="age" value={userDetails.age} onChange={handleChange} />
+        <label className="label">Gender</label>
+        <input className="input" type="text" name="gender" value={userDetails.gender} onChange={handleChange} />
+        <input className="button" type="submit" value="Add User" />
+      </form>
+    </>
+  );
+};
 
-export default AddUser
+export default AddUser;
